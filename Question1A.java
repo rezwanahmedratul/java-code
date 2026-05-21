cd9b18972e06a972028c059d702899754f30aca7
Rezwan Ahmed Ratul <rezwanahmedratul007@gmail.com>
2026-05-21 14:14:57 +0600

    Add palindrome checker implementation in Question1A class

---
+++ Question1A.java
@@ -0,0 +1,24 @@
+public class Question1A {
+    public static boolean palindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        boolean isPalindrome = Question1A.palindrome(str);
+        if(isPalindrome) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
